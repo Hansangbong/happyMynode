df143367b1d4f2fe0af601b9d31e508813b062ef
snagbongHan <hsbzzang3@gmail.com>
2024-04-24 17:30:42 +0900

    happy 2

--- myContacts/quiz/quiz-07-3.js
+++ myContacts/quiz/quiz-07-3.js
@@ -1,6 +1,31 @@
 // '/' 경로로 GET 요청했을 때 사용자 정보(users)를 JSON 형태로 응답하는 미들웨어를 작성하세요.
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
 const users = [
   { id: 1, name: "Kim" },
   { id: 2, name: "Lee" },
   { id: 3, name: "Park" },
 ];
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello");
+});
+
+
+app.get("/users", (req, res) => {
+  res.json(users); 
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`${port}번 포트에서 quiz7-3 실행 중`)
+});
--- myContacts/quiz/quiz-07-4.js
+++ myContacts/quiz/quiz-07-4.js
@@ -1,2 +1,26 @@
 // '/about' 경로로 GET 요청이 들어왔을 때 "This is the About page"라는 메시지를 보내는 라우터 미들웨어를 추가해보세요.
 // Router 객체를 사용해서 작성하세요.
+const express = require("express");
+const router = express.Router();
+const app=express();
+const port = 3000;
+
+
+app.use(router);
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello!");
+})
+
+router.route("/about").get((req,res) => {
+    res.status(200).send("This is the About page");
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`${port}번 포트에서 quiz 작동 중`);
+})
